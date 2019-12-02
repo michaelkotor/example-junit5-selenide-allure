6cd6c23d677e0a37a41560b88c4150dcdc733ff8
michaelkotor <milan3020@gmail.com>
2019-12-02 16:52:12 +0300

    create tests with different errors

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,7 +1,21 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class TestClass {
+    @BeforeAll
+    public static void config() {
+        Configuration.screenshots = true;
+        Configuration.savePageSource = false;
+        Configuration.timeout = 10000;
+        Configuration.browser = "chrome";
+        Configuration.headless = false;
+        Configuration.browserSize = "1920x1080";
+    }
+
     @Test
     public void testPass() {
         Assertions.assertTrue(true);
@@ -10,5 +24,38 @@ public class TestClass {
     @Test
     public void testFail() {
         Assertions.assertTrue(false);
+    }
+
+    @Test
+    public void selenideElementExistsPass() {
+        Selenide.open("http://google.com");
+        SelenideElement selenideElement = Selenide.element("div");
+        Assertions.assertNotNull(selenideElement);
+    }
+
+    @Test
+    public void selenideElementIsNotFoundFail() {
+        Selenide.open("https://google.com");
+        SelenideElement selenideElement = Selenide.element("michael");
+        Assertions.assertTrue(selenideElement.exists());
+    }
+
+    @Test
+    public void selenideElementErrorBeforeAssertFail() {
+        Selenide.open("https://google.com");
+        SelenideElement selenideElement = Selenide.element("michael");
+        selenideElement.click();
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void selenideElementErrorBeforeErrorAssertFail() {
+        Selenide.open("https://google.com");
+        SelenideElement selenideElement = Selenide.element("michael");
+        if(true) {
+            throw new RuntimeException();
+        }
+        selenideElement.click();
+        Assertions.assertTrue(true);
     }
 }
